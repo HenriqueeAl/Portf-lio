7a74e4af2f6b1d3aa42d6c4c9f37c468f70ad326
henrique <henrique.almeida231@hotmail.com>
2022-12-11 21:53:34 -0300

    projetos update 1

--- src/components/projetos/projetos.tsx
+++ src/components/projetos/projetos.tsx
@@ -7,27 +7,30 @@ export const Projetos = () => {
             <Projeto 
             name='Todolist App'
             repo='https://github.com/HenriqueeAl/Todolist-crud'
-            tecnologias={['HTML', 'CSS', 'SASS','JavaScript','TypeScript', 'React', 'Node']}
+            tecnologias={['HTML', 'CSS', 'SASS','JavaScript','TypeScript', 'React', 'Node', 'Prisma']}
             site='https://todolist-crud.vercel.app/'
             bg='url(todolist.png)'
-            descricao='Um app de que voce pode criar uma conta e adicionar tarefas do seu dia a dia, marcar como concluidas, editalas e deletalas.'
+            descricao='Um app que voce pode criar uma conta e adicionar tarefas do seu dia a dia, marcar como concluidas, editalas e deletalas.'
             />
             <Projeto name='Blizzard'
-            tecnologias={['HTML', 'CSS', 'SASS', 'React']}
+            tecnologias={['HTML', 'CSS', 'SASS', 'JavaScript','TypeScript', 'React']}
+            repo='https://github.com/HenriqueeAl/blizzard'
             site='https://blizzard-iota.vercel.app/'
             bg='url(projectbilzzard.png)'
             descricao='Um app de que voce pode criar uma conta e adicionar tarefas do seu dia a dia, marcar como concluidas, editalas e deletalas.'
             />
             <Projeto name='Pizza Time'
-            tecnologias={['HTML']}
-            site='https://todolist-crud.vercel.app/'
-            bg='url(todolist.png)'
+            tecnologias={['HTML', 'CSS', 'SASS', 'JavaScript','TypeScript', 'React']}
+            repo='https://github.com/HenriqueeAl/Pizza-Time'
+            site='https://pizza-time-five.vercel.app/'
+            bg='url(pizzatimebg.png)'
             descricao='Um app de que voce pode criar uma conta e adicionar tarefas do seu dia a dia, marcar como concluidas, editalas e deletalas.'
             />
             <Projeto name='Burger Empire'
-            tecnologias={['HTML']}
-            site='https://todolist-crud.vercel.app/'
-            bg='url(todolist.png)'
+            tecnologias={['HTML', 'CSS', 'SASS', 'JavaScript','TypeScript', 'React']}
+            repo='https://github.com/HenriqueeAl/Empire-Burger---BRChallenges'
+            site='https://empire-burger-brchallenges.vercel.app/'
+            bg='url(empireburgerbg.png)'
             descricao='Um app de que voce pode criar uma conta e adicionar tarefas do seu dia a dia, marcar como concluidas, editalas e deletalas.'
             />
         </div>
